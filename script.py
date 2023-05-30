81ca024e515e1aab076a18e25776af120f00756b
linnnus <linnnus@users.noreply.github.com>
2023-05-30 06:25:05 +0000

    Initial commit

---
+++ script.py
@@ -0,0 +1,106 @@
+import datetime
+import dotenv
+import os
+import pytz
+import requests
+import sys
+
+## Config
+
+dotenv.load_dotenv()
+
+# The id of the webhook to use.
+WEBHOOK_ID = os.environ["WEBHOOK_ID"]
+
+# The token of the webhook to use.
+WEBHOOK_TOKEN = os.environ["WEBHOOK_TOKEN"]
+
+# A mapping from week numbers to lists of names. This is used
+# to look up, whose turn it is.
+week_to_suckers = {
+    11: ["***REMOVED***", "***REMOVED***"],
+    12: ["***REMOVED***", "***REMOVED***"],
+    13: ["***REMOVED***", "***REMOVED***"],
+    15: ["***REMOVED***", "***REMOVED***"],
+    16: ["***REMOVED***", "***REMOVED***"],
+    17: ["***REMOVED***", "***REMOVED***"],
+    18: ["***REMOVED***", "***REMOVED***"],
+    19: ["***REMOVED***", "***REMOVED***"],
+    20: ["***REMOVED***", "***REMOVED***"],
+    21: ["***REMOVED***", "***REMOVED***"],
+    22: ["***REMOVED***", "***REMOVED***"]
+}
+
+# A mapping from names to user ids. This is used to ping members.
+name_to_id = {
+    "***REMOVED***":  "386838704184098818",
+    "***REMOVED***":  "386838704184098818",
+    "***REMOVED***":              "377142356354269184",
+    "***REMOVED***":                  "727113666788327475",
+    "***REMOVED***":                 "444574998942711808",
+    "***REMOVED***":              "285867862588129283",
+    "***REMOVED***":          "318057147940470795",
+    "***REMOVED***":               "763332469654028328",
+    "***REMOVED***":                  "1042082836368343100",
+    "***REMOVED***":       "281387674588151808",
+    "***REMOVED***":        "590912103339130891",
+    "***REMOVED***":          "907187868819148801",
+    "***REMOVED***":             "651332923911438340",
+    "***REMOVED***":          "374086040874385412",
+    "***REMOVED***":         "351795098075398145",
+    "***REMOVED***":             "431848896000425994",
+    "***REMOVED***":       "418799061571731458",
+    "***REMOVED***":     "464712436528840725",
+    "***REMOVED***": "585394887176880128",
+    "***REMOVED***":            "629682837804810270",
+    "***REMOVED***":         "366147716851433474",
+    "***REMOVED***":             "518109475178020864",
+    "***REMOVED***":    "405001313269710871",
+    "***REMOVED***":            "591616772818337802",
+    "***REMOVED***":       "291897381341888513",
+    "***REMOVED***":           "216621385173172225",
+    "***REMOVED***":           "366257424346251264",
+    "***REMOVED***":   "311544259662577675",
+}
+
+# Time zone to use for current week.
+TIMEZONE = "Europe/Copenhagen"
+
+## Who's turn is it?
+
+time_zone = pytz.timezone(TIMEZONE)
+today = datetime.datetime.now(time_zone).date()
+try:
+    week = today.isocalendar().week
+except IndexError:
+    print("No suckers for this week. Exiting.")
+    sys.exit(0)
+
+names = week_to_suckers[week]
+
+## Construct message
+
+def try_mention(name):
+    try:
+        id = name_to_id[name]
+        return f"{name} (<@{id}>)"
+    except KeyError:
+        return name
+
+mentions = [try_mention(n) for n in names]
+
+concatenated_mentions = ", ".join(mentions[:-1]) + " og " + mentions[-1]
+
+message = f"Der er IT hjælp i lokale D4407\n(dagens dukse er {concatenated_mentions}\nnu vil jeg ikke have nogen skipperering fra jer gutter og mia!)"
+
+## Send data
+
+url = f"https://discord.com/api/webhooks/{WEBHOOK_ID}/{WEBHOOK_TOKEN}"
+
+data = {
+        "username": "Duksebot",
+        "content": message,
+}
+
+response = requests.post(url, json=data)
+response.raise_for_status()
