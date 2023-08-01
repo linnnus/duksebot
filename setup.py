c593bfbd9a051c70b65c04d8741cb5b2e7c39cad
linnnus <linnnus@users.noreply.github.com>
2023-08-01 11:00:44 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="duksebot",
+      version="0.1",
+      description="github.com/linnnus/duksebot",
+      author="Linus overduks",
+      url="https://github.com/linnnus/duksebot",
+      scripts=["./script.py"],
+     )
